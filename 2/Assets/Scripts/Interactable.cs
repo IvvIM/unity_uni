6a3a77113f7b9544fa8f5b7e34af8bf5ba7436e9
Ivv <ivv.jpeg@gmail.com>
2020-11-30 02:13:07 +0200

    Added start of 2 lab

---
+++ 2/Assets/Scripts/Interactable.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    protected virtual void FixedUpdate()
+    {
+    }
+
+    public virtual void ConfirmInteract()
+    {
+        //Метод який викликається коли гравець починає взаємодіяти з сутністю
+        InInteract();
+    }
+
+    public virtual void DeclineInteract()
+    {
+        //Метод який віклікаеться коли гравець припиняє взаємодіяти з сутністю
+        OutInteract();
+    }
+
+    protected virtual bool PlayerCanInteract()
+    {
+        //Перевірка чи гравець може взаємодіяти з сутністю
+    }
+
+    protected virtual bool IsPlayer()
+    {
+        //Перевірка на гравця в зоні дій
+    }
+
+    protected virtual void OnPlayerEnter()
+    {
+        //Викликає деякі методи при вході гравця в зону дій
+    }
+
+    protected virtual void OnTriggerExit()
+    {
+        //Викликає деякі методи при виході гравця з зони дій
+    }
+
+    protected virtual void InInteract()
+    {
+        //Вхід в стан взаємодії
+    }
+
+    protected virtual void OutInteract()
+    {
+        //Вихід з стану взаємодії
+    }
+}
+
